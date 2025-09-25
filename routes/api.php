2e9eaf9a428757d38907054fa755d488775cc30e
Budi Imam Prasetyo <budiimamprsty@gmail.com>
2025-09-25 13:57:49 +0700

    Refactor project configuration and add Laravel Sanctum for API authentication

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-
-// routes/api.php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::get('/posts', function () {
     return [
